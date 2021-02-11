9e67b0702700d8827182fbd8590a69060cbf724b
NameTrain <nam.tran1@stu.pfisd.net>
2021-02-11 22:25:30 +0000

    Added test.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ headers = {
     'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'
 }
+
 url = input("Type a Reddit Url (Ex. https://www.reddit.com/r/birds/): ")
 response = requests.get(url, headers=headers)
-
 soup = BeautifulSoup(response.content, 'lxml')
 
-
-for item in soup.select('.Post'):
+#Get the stuff from Reddit website
+def get_info_reddit():
+  for item in soup.select('.Post'):
     try:
         print('----------------------------------------')
         print(item.select('._1rZYMD_4xY3gRcSS3p8ODO')[0].get_text())
@@ -22,3 +23,12 @@ for item in soup.select('.Post'):
     except Exception as e:
         #raise e
         print('')
+get_info_reddit()
+
+
+#Loops many time for user to input diff. links
+while True:
+  url = input("Type another Reddit Url: ")
+  response = requests.get(url, headers=headers)
+  soup = BeautifulSoup(response.content, 'lxml')
+  get_info_reddit()
